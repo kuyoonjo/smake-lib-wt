025b797cc6e0de2da6d82f5b0e1a4018321ff49d
Yu Chen <yu.chen@live.ie>
2021-08-03 16:05:51 +0800

    sep

--- __tests__/wt-spec.ts
+++ __tests__/wt-spec.ts
@@ -1,7 +1,7 @@
 import { wt } from '../src/wt';
 import { LLVM_Darwin } from 'smake';
 import { addLibs } from '@smake/libs';
-import { resolve, sep } from 'path';
+import { resolve } from 'path';
 
 test('wt', () => {
   class A extends LLVM_Darwin {
@@ -9,14 +9,8 @@ test('wt', () => {
   }
   const B = addLibs(A, wt, wt, wt);
   const b = new B();
-  const p1 = resolve(__dirname, '..', 'wt', 'include').replace(
-    new RegExp(sep, 'g'),
-    '/'
-  );
-  const p2 = resolve(__dirname, '..', 'wt', 'include').replace(
-    new RegExp(sep, 'g'),
-    '/'
-  );
+  const p1 = resolve(__dirname, '..', 'wt', 'include').replace(/\\/g, '/');
+  const p2 = resolve(__dirname, '..', 'wt', 'include').replace(/\\/g, '/');
   expect(b.sysIncludedirs.includes(p1)).toBe(true);
   expect(b.sysIncludedirs.includes(p2)).toBe(true);
   expect(b.sysIncludedirs.length).toBe(6);
--- src/wt.ts
+++ src/wt.ts
@@ -1,18 +1,12 @@
-import { sep, resolve } from 'path';
+import { resolve } from 'path';
 import { LLVM } from 'smake';
 
 export function wt(t: LLVM) {
   Object.defineProperty(t, 'sysIncludedirs', {
     value: [
       ...t.sysIncludedirs,
-      resolve(__dirname, '..', 'wt', 'include').replace(
-        new RegExp(sep, 'g'),
-        '/'
-      ),
-      resolve(__dirname, '..', 'MPMCQueue', 'include').replace(
-        new RegExp(sep, 'g'),
-        '/'
-      ),
+      resolve(__dirname, '..', 'wt', 'include').replace(/\\/g, '/'),
+      resolve(__dirname, '..', 'MPMCQueue', 'include').replace(/\\/g, '/'),
     ],
     configurable: true,
   });
